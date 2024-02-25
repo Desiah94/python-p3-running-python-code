d5bf6849ae00c7fb09a994d1280b146420ede183
Desiah Barnett <desiahbarnett@gmail.com>
2024-02-24 20:27:58 -0500

    updated notes 2/24/2024

--- lib/app.py
+++ lib/app.py
@@ -1,2 +1,32 @@
 # app.py
 print("Hello World! Pass this test, please.")
+
+# Run the application type python lib/app.py into the terminal
+
+#print("Hello world!")
+#print("Hello sun!")
+#print("Hello sky!")
+
+# Choosing your own print() Ending
+# run the text side paragraph style/ not line by line
+#print("Hello world!", end=" ")
+#print("Hello sun!", end="!! ")
+#print("Hello sky!", end="!!!\n")
+# run the application again using python lib/app.py
+
+
+# Exploring Python with the Python Interpreter
+#Using Python shell for running Python REPL 
+#to test out code, check syntax without running app
+
+# Run Python Shell
+# Type Python into the terminal
+# type print("Hello Python shell")
+# type first_number = 7
+# type print(first_number)
+
+#Running pytest Tests
+# Install pytest pipenv install run this after git clone
+# virtual environment will activate
+# type pipenv shell to start working 
+
